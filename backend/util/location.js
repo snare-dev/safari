71198ecb3bd47c27ef4850465e47aa10c2a6e73b
snare-dev <amanisanare1@gmail.com>
2023-01-21 13:15:46 -0500

    removed keys

--- backend/util/location.js
+++ backend/util/location.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const HttpError = require('../models/http-error');
 
-const API_KEY = '';
+const API_KEY = process.env.GOOGLE_GEOLOCATION;
 
 async function getCoordsForAddress(address) {
   // return {
